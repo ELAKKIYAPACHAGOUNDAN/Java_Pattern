45a6b6fd9ba2391f53712a07570eef16460eeb81
Elakkiya <141120027+ELAKKIYAPACHAGOUNDAN@users.noreply.github.com>
2025-05-26 22:10:20 +0530

    pattern_practice

--- Fivefive_Pattern.java
+++ Fivefive_Pattern.java
@@ -3,10 +3,65 @@ public class Fivefive_Pattern {
 	public static void main(String[] args) {
 		for(int i=1; i<=5; i++) {
 			for(int j=1; j<=5; j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		/* output
+		1 1 1 1 1 
+		2 2 2 2 2 
+		3 3 3 3 3 
+		4 4 4 4 4 
+		5 5 5 5 5 */
+		
+		System.out.println();
+
+		for(int i=1; i<=5; i++) {
+			for(int j=1; j<=5; j++) {
 				System.out.print(j+" ");
 			}
-			System.out.println() ;
-
+			System.out.println();
 		}
+		/* output
+		1 2 3 4 5 
+		1 2 3 4 5 
+		1 2 3 4 5 
+		1 2 3 4 5 
+		1 2 3 4 5 */
+		
+		System.out.println();
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=5; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		/*output
+		* * * * * 
+		* * * * * 
+		* * * * * 
+		* * * * * 
+		* * * * *
+		 */
+		System.out.println();
+		for (int i=1; i<=5; i++) {
+			for (int j=1; j<=5; j++) {
+				if(i==1 || i==5 || j==1 || j==5) {
+					System.out.print("* ");
+				}
+				else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
+		}
+		/*output
+		 	* * * * * 
+			*       * 
+			*       * 
+			*       * 
+			* * * * * 
+		 */
+		
 	}
 }
